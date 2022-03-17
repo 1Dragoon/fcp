dd8df64f42b405960e03d93d745688cd0dda1951
j <o>
2022-03-16 23:17:53 -0700

    trying something else

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use clap::Parser;
 use indicatif::{FormattedDuration, HumanBytes, ProgressBar, ProgressStyle};
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use rayon::Scope;
-
 use std::fs::{self, DirEntry, Metadata};
 use std::path::{Path, PathBuf};
 use std::sync::atomic::{AtomicBool, AtomicU64, AtomicUsize, Ordering};
@@ -54,6 +53,7 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+#[allow(clippy::option_map_unit_fn)]
 fn copy_recurse<U: AsRef<Path>, V: AsRef<Path>>(source: &U, dest: &V) -> Result<()> {
     let start = std::time::Instant::now();
 
@@ -73,12 +73,21 @@ fn copy_recurse<U: AsRef<Path>, V: AsRef<Path>>(source: &U, dest: &V) -> Result<
 
     let state = work(source, &pb, dest);
 
-    let sizes = state
+    let successful_copy_sizes = state
         .complete_rx
         .into_iter()
-        .filter_map(|r| r.size)
+        .filter_map(|r| match r {
+            JobResult::CopySuccess(s) => Some(s.size),
+            JobResult::ScanFileFailure(_) => None,
+            JobResult::ScanDirFailure(_) => None,
+            JobResult::ScanSuccess(_) => None,
+            JobResult::ScanPartialSuccess(_) => None,
+            JobResult::CopyFailure(_) => None,
+            JobResult::CopyPartialSuccess(s) => s.size,
+            JobResult::PermaFailure(s) => s.size,
+        })
         .collect::<Vec<_>>(); // Block here until complete_tx sender is gone
-    let copied_data = sizes.iter().sum();
+    let copied_data = successful_copy_sizes.iter().sum();
 
     let perm_failed = state.failed_rx.into_iter().collect::<Vec<_>>(); // Block here until the err_thread sender is gone
 
@@ -88,12 +97,23 @@ fn copy_recurse<U: AsRef<Path>, V: AsRef<Path>>(source: &U, dest: &V) -> Result<
     let _ = pb;
 
     if !perm_failed.is_empty() {
-        perm_failed
-            .into_iter()
-            .for_each(|f| println!("{:?}", f.error));
+        perm_failed.into_iter().for_each(|r| {
+            let err = match r {
+                JobResult::ScanFileFailure(a) => Some(a.error),
+                JobResult::ScanDirFailure(a) => Some(a.error),
+                JobResult::ScanSuccess(_) => None,
+                JobResult::ScanPartialSuccess(a) => Some(a.error),
+                JobResult::CopyFailure(a) => Some(a.error),
+                JobResult::CopySuccess(_) => None,
+                JobResult::CopyPartialSuccess(_) => None,
+                JobResult::PermaFailure(a) => Some(a.error),
+            };
+
+            err.map(|e| println!("{:?}", e));
+        });
         println!(
             "Copied {} files of {}, {} of {} in {}",
-            sizes.len(),
+            successful_copy_sizes.len(),
             state.file_count.load(Ordering::Relaxed),
             HumanBytes(copied_data),
             HumanBytes(state.total_size.load(Ordering::Relaxed)),
@@ -112,10 +132,10 @@ fn copy_recurse<U: AsRef<Path>, V: AsRef<Path>>(source: &U, dest: &V) -> Result<
 }
 
 struct State {
-    failed_rx: mpsc::Receiver<ActionResult>,
+    failed_rx: mpsc::Receiver<JobResult>,
     file_count: Arc<AtomicUsize>,
     total_size: Arc<AtomicU64>,
-    complete_rx: mpsc::Receiver<ActionResult>,
+    complete_rx: mpsc::Receiver<JobResult>,
     copy_thread: thread::JoinHandle<()>,
     err_thread: thread::JoinHandle<()>,
 }
@@ -141,6 +161,7 @@ fn work(source: PathBuf, pb: &ProgressBar, dest: PathBuf) -> State {
     let copy_finished = Arc::new(AtomicBool::new(false));
     let copy_thread = {
         let etx = error_tx;
+        let ftx = failed_tx.clone();
         let ctx = complete_tx.clone();
         let cp_fin = copy_finished.clone();
         let cp_pb = pb.clone();
@@ -152,24 +173,23 @@ fn work(source: PathBuf, pb: &ProgressBar, dest: PathBuf) -> State {
                 let done = scan_finished.load(Ordering::Relaxed);
                 thread::sleep(Duration::new(0, 100_000_000)); // Sleep 100ms to give the CPU a coffee break
                 let scan_results = scan_rx.try_iter().into_iter().collect::<Vec<_>>();
-                let mut scan_err_set = Vec::new();
                 let mut file_set = Vec::new();
                 for scan_result in scan_results {
-                    if scan_result.has_error() {
-                        if scan_result.dir.is_some() {
-                            etx.send(scan_result).unwrap();
-                        } else {
-                            scan_err_set.push(scan_result)
-                        }
-                    } else {
-                        file_set.push(scan_result);
+                    match scan_result {
+                        JobResult::ScanFileFailure(_) => ftx.send(scan_result).unwrap(),
+                        JobResult::ScanDirFailure(_) => ftx.send(scan_result).unwrap(),
+                        JobResult::ScanSuccess(_) => file_set.push(scan_result),
+                        JobResult::ScanPartialSuccess(_) => file_set.push(scan_result),
+                        JobResult::CopyFailure(_) => file_set.push(scan_result),
+                        JobResult::CopySuccess(_) => ctx.send(scan_result).unwrap(),
+                        JobResult::CopyPartialSuccess(_) => ctx.send(scan_result).unwrap(),
+                        JobResult::PermaFailure(_) => ftx.send(scan_result).unwrap(),
                     }
                 }
                 if !file_set.is_empty() {
                     cp_fc.fetch_add(file_set.len(), Ordering::Relaxed);
                     process_files(&cp_pb, &cp_src, &cp_dst, file_set, etx.clone(), ctx.clone());
                 }
-                scan_err_set.into_iter().for_each(|r| etx.send(r).unwrap());
                 if done {
                     break;
                 }
@@ -190,14 +210,15 @@ fn work(source: PathBuf, pb: &ProgressBar, dest: PathBuf) -> State {
                 let copy_results = error_rx.try_iter().into_iter().collect::<Vec<_>>();
                 let mut retry_set = Vec::new();
                 for copy_result in copy_results {
-                    if copy_result.has_error() {
-                        if copy_result.dir.is_some() {
-                            ftx.send(copy_result).unwrap();
-                        } else {
-                            retry_set.push(copy_result);
-                        }
-                    } else {
-                        retry_set.push(copy_result);
+                    match copy_result {
+                        JobResult::ScanFileFailure(_) => ftx.send(copy_result).unwrap(),
+                        JobResult::ScanDirFailure(_) => ftx.send(copy_result).unwrap(),
+                        JobResult::ScanSuccess(_) => retry_set.push(copy_result),
+                        JobResult::ScanPartialSuccess(_) => retry_set.push(copy_result),
+                        JobResult::CopyFailure(_) => retry_set.push(copy_result),
+                        JobResult::CopySuccess(_) => ctx.send(copy_result).unwrap(),
+                        JobResult::CopyPartialSuccess(_) => ctx.send(copy_result).unwrap(),
+                        JobResult::PermaFailure(_) => ftx.send(copy_result).unwrap(),
                     }
                 }
 
@@ -267,9 +288,9 @@ fn process_files<U: AsRef<Path> + Sync, V: AsRef<Path> + Sync>(
     pb: &ProgressBar,
     source: &U,
     dest: &V,
-    files: Vec<ActionResult>,
-    failed_tx: Sender<ActionResult>,
-    complete_tx: Sender<ActionResult>,
+    files: Vec<JobResult>,
+    failed_tx: Sender<JobResult>,
+    complete_tx: Sender<JobResult>,
 ) {
     files
         .into_par_iter()
@@ -277,35 +298,58 @@ fn process_files<U: AsRef<Path> + Sync, V: AsRef<Path> + Sync>(
             (senders.clone(), result)
         })
         .for_each(|f| {
-            let (senders, mut scan_result) = f;
+            let (senders, scan_result) = f;
             let (failed_tx, complete_tx) = senders;
 
-            if scan_result.file.is_some() {
-                let outcome = copy_file(source, dest, &mut scan_result, pb);
-                let err = outcome.err();
-                if err.is_some() {
-                    failed_tx.send(scan_result).unwrap();
-                } else {
-                    scan_result.error = err;
-                    complete_tx.send(scan_result).unwrap();
-                }
+            let mut ji = JobInfo::new();
+
+            let can_process = match scan_result {
+                JobResult::ScanFileFailure(_) => panic!(), // Shouldn't happen
+                JobResult::ScanDirFailure(_) => panic!(),  // Shouldn't happen
+                JobResult::ScanSuccess(a) => { ji.dir_entry = Some(a.dir_entry); ji.metadata = Some(a.meta_data); ji.has_md = true; ji.size = Some(a.size); true },
+                JobResult::ScanPartialSuccess(a) => { ji.dir_entry = Some(a.dir_entry); true  }
+                JobResult::CopyFailure(a) => { ji.dir_entry = Some(a.dir_entry); ji.error.extend(a.error); ji.metadata = a.meta_data; ji.has_md = true; ji.size = a.size; true },
+                JobResult::CopySuccess(_) => panic!(), // Shouldn't happen
+                JobResult::CopyPartialSuccess(_) => panic!(), // Shouldn't happen
+                JobResult::PermaFailure(a) => { ji.dir_entry = a.dir_entry; ji.error.extend(a.error); ji.metadata = a.meta_data; ji.has_md = true; ji.size = a.size; false },
+            };
+
+            if can_process {
+                let jobresult = copy_file(source, dest, &mut ji, pb);
+                match jobresult {
+                    Ok(r) => complete_tx.send(r).unwrap(),
+                    Err(err) => 
+                    { let error = vec![err];
+                        failed_tx
+                        .send(JobResult::CopyFailure(CopyFailure {
+                            error,
+                            dir_entry: ji.dir_entry.unwrap(),
+                            meta_data: ji.metadata,
+                            size: ji.size,
+                        }))
+                        .unwrap()},
+                };
             } else {
-                failed_tx.send(scan_result).unwrap();
+                let jobresult = JobResult::PermaFailure(PermaFailure{
+                    error: ji.error,
+                    dir_entry: ji.dir_entry,
+                    meta_data: ji.metadata,
+                    size: ji.size,
+                });
+                failed_tx.send(jobresult).unwrap();
             }
-        });
+
+
+        })
 }
 
 fn copy_file<U: AsRef<Path> + Sync, V: AsRef<Path> + Sync>(
     source: &U,
     dest: &V,
-    copy_result: &mut ActionResult,
+    process: &mut JobInfo,
     pb: &ProgressBar,
-) -> Result<(), anyhow::Error> {
-    // Just need to borrow this for a second...
-    let direntry = copy_result.file.take().unwrap();
-    let spath = direntry.path();
-    // And let's put it back where we found it... :)
-    copy_result.file = Some(direntry);
+) -> Result<JobResult, anyhow::Error> {
+    let spath = process.dir_entry.as_ref().unwrap().path();
 
     let s = spath.display().to_string();
     if let Some((i, _)) = s.char_indices().rev().nth(40) {
@@ -315,16 +359,22 @@ fn copy_file<U: AsRef<Path> + Sync, V: AsRef<Path> + Sync>(
     let dpath = dest.as_ref().join(stem);
 
     // If we don't have the size, make sure we have the metadata. If we don't have the metadata and still can't get it, leave both it and size to None.
-    if copy_result.size.is_none() {
-        copy_result.metadata = copy_result
-            .metadata
-            .take()
-            .or_else(|| spath.metadata().ok())
-            .map(|md| {
-                // If we can get the metadata, set both it and the size fields.
-                copy_result.size = Some(md.len());
-                md
-            });
+    if process.size.is_none() {
+        if let Some(metadata) = &process.metadata {
+            process.has_md = true;
+            process.size = Some(metadata.len());
+        } else {
+            process.metadata = process
+                .metadata
+                .take()
+                .or_else(|| spath.metadata().ok())
+                .map(|md| {
+                    // If we can get the metadata, set both it and the size fields.
+                    process.size = Some(md.len());
+                    md
+                });
+            process.has_md = true;
+        }
     }
 
     if dpath.exists() {
@@ -340,47 +390,121 @@ fn copy_file<U: AsRef<Path> + Sync, V: AsRef<Path> + Sync>(
 
     #[cfg(target_os = "windows")]
     let transferred = win_stuff::win_copy(&spath, &dpath, pb)?;
-    if copy_result.size.is_some() {
+    if process.size.is_some() {
         // let size = copy_result.size.unwrap();
         // if size != transferred {
         // println!("Liar!");
         // }
     } else {
-        copy_result.size = Some(transferred);
+        process.size = Some(transferred);
     }
 
     #[cfg(not(target_os = "windows"))]
     {
         fs::copy(&spath, &dpath)?;
-        if copy_result.size.is_some() {
-            let size = copy_result.size.unwrap();
+        if process.size.is_some() {
+            let size = process.size.unwrap();
             pb.inc(size);
         }
     }
 
-    Ok(())
+    process.success = true;
+
+    let jobresult = if process.has_md {
+        JobResult::CopySuccess(CopySuccess {
+            dir_entry: process.dir_entry.take().unwrap(),
+            meta_data: process.metadata.take().unwrap(),
+            size: process.size.unwrap(),
+        })
+    } else {
+        JobResult::CopyPartialSuccess(CopyPartialSuccess {
+            dir_entry: process.dir_entry.take().unwrap(),
+            meta_data: process.metadata.take(),
+            size: process.size,
+        })
+    };
+
+    Ok(jobresult)
 }
 
-struct ActionResult {
-    error: Option<anyhow::Error>,
-    file: Option<DirEntry>,
-    dir: Option<PathBuf>,
-    metadata: Option<Metadata>,
+// Add backoff timer to copy failures? Re-send failed dir scans back into the scanner, also with backoff timer?
+enum JobResult {
+    ScanFileFailure(ScanFileFailure),
+    ScanDirFailure(ScanDirFailure),
+    ScanSuccess(ScanSuccess),
+    ScanPartialSuccess(ScanPartialSuccess),
+    CopyFailure(CopyFailure),
+    CopySuccess(CopySuccess),
+    CopyPartialSuccess(CopyPartialSuccess),
+    PermaFailure(PermaFailure),
+}
+
+struct ScanPartialSuccess {
+    error: Vec<anyhow::Error>,
+    dir_entry: DirEntry,
+}
+
+struct CopyFailure {
+    error: Vec<anyhow::Error>,
+    dir_entry: DirEntry,
+    meta_data: Option<Metadata>,
     size: Option<u64>,
 }
 
-impl ActionResult {
-    fn has_error(&self) -> bool {
-        self.error.is_some()
-    }
+struct PermaFailure {
+    error: Vec<anyhow::Error>,
+    dir_entry: Option<DirEntry>,
+    meta_data: Option<Metadata>,
+    size: Option<u64>,
+}
 
-    fn new() -> ActionResult {
-        ActionResult {
-            error: None,
-            file: None,
+struct CopySuccess {
+    dir_entry: DirEntry,
+    meta_data: Metadata,
+    size: u64,
+}
+struct CopyPartialSuccess {
+    dir_entry: DirEntry,
+    meta_data: Option<Metadata>,
+    size: Option<u64>,
+}
+
+struct ScanFileFailure {
+    error: Vec<anyhow::Error>,
+    dir_entry_result: Result<DirEntry, std::io::Error>,
+}
+
+struct ScanDirFailure {
+    error: Vec<anyhow::Error>,
+    dir: PathBuf,
+}
+
+struct ScanSuccess {
+    dir_entry: DirEntry,
+    meta_data: Metadata,
+    size: u64,
+}
+
+struct JobInfo {
+    error: Vec<anyhow::Error>,
+    dir_entry: Option<DirEntry>,
+    dir: Option<PathBuf>,
+    metadata: Option<Metadata>,
+    size: Option<u64>,
+    success: bool,
+    has_md: bool,
+}
+
+impl JobInfo {
+    fn new() -> JobInfo {
+        JobInfo {
+            error: Vec::new(),
+            dir_entry: None,
             dir: None,
             metadata: None,
             size: None,
+            success: false,
+            has_md: false,
         }
     }
 }
@@ -388,47 +512,62 @@ impl ActionResult {
 fn scan<'a, U: AsRef<Path>>(
     pb: ProgressBar,
     src: &U,
-    tx: Sender<ActionResult>,
+    tx: Sender<JobResult>,
     total_size: Arc<AtomicU64>,
     scope: &Scope<'a>,
 ) {
     match fs::read_dir(src) {
         Ok(dir) => {
             dir.into_iter().for_each(|dir_entry_result| {
-                let mut result = ActionResult::new();
+                // let mut result = ActionResult::new();
                 match dir_entry_result {
-                    Ok(entry) => {
-                        let path = entry.path();
-                        result.file = Some(entry);
+                    Ok(dir_entry) => {
+                        let path = dir_entry.path();
                         if path.is_dir() {
                             let tx = tx.clone();
                             let pb = pb.clone();
                             let ts = total_size.clone();
                             scope.spawn(move |s| scan(pb, &path, tx, ts, s))
                         } else {
-                            if let Ok(md) = path.metadata() {
-                                let size = md.len();
-                                pb.inc_length(size);
-                                total_size.fetch_add(size, Ordering::Relaxed);
-                                result.size = Some(md.len());
-                                result.metadata = Some(md);
-                            } else {
-                                result.error = Some(anyhow!("Failed to read metadata"));
+                            match path.metadata() {
+                                Ok(meta_data) => {
+                                    let size = meta_data.len();
+                                    pb.inc_length(size);
+                                    total_size.fetch_add(size, Ordering::Relaxed);
+                                    let result = JobResult::ScanSuccess(ScanSuccess {
+                                        dir_entry,
+                                        meta_data,
+                                        size,
+                                    });
+                                    tx.send(result).unwrap();
+                                }
+                                Err(err) => {
+                                    let error = vec![anyhow!("Failed to read metadata: {}", err)];
+                                    let result =
+                                        JobResult::ScanPartialSuccess(ScanPartialSuccess {
+                                            error,
+                                            dir_entry,
+                                        });
+                                    tx.send(result).unwrap();
+                                }
                             }
-                            tx.send(result).unwrap();
                         }
                     }
-                    Err(_) => {
-                        result.error = Some(anyhow!("Failed to get directory entry"));
+                    Err(ref err) => {
+                        let error = vec![anyhow!("Failed to get directory entry: {}", err)];
+                        let result = JobResult::ScanFileFailure(ScanFileFailure {
+                            error,
+                            dir_entry_result,
+                        });
                         tx.send(result).unwrap();
                     }
                 }
             });
         }
-        Err(_) => {
-            let mut result = ActionResult::new();
-            result.error = Some(anyhow!("Failed to read from directory"));
-            result.dir = Some(src.as_ref().to_path_buf());
+        Err(err) => {
+            let error = vec![anyhow!("Failed to read from directory: {}", err)];
+            let dir = src.as_ref().to_path_buf();
+            let result = JobResult::ScanDirFailure(ScanDirFailure { error, dir });
             tx.send(result).unwrap();
         }
     }
